fd3fcc4cefdc4db74c22572095024a691ffec258
Mariana Dominguez <maridoh@gmail.com>
2020-06-09 20:29:15 -0700

    get and post Posts is working

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const Post = require('../models/Post')
 
 const app = express();
 app.use(morgan('combined'));
@@ -18,18 +20,42 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => console.log("Connection to DB Succeeded"))
 
+// Get posts
 app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: "Hello world",
-      description: "hi there, howdy do?"
-    },
-    {
-      title: "Heya",
-      description: "hi there, good and u?"
-    }]
-  )
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error); }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1})
 })
+
+// Add new post
+app.post('/posts', (req, res) => {
+  const db = req.db;
+  const title = req.body.title;
+  const description = req.body.description;
+
+  const new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+
+
+
+})
+
+
 
 
 
