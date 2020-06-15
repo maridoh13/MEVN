a3df2a02e8057da950423f9536aca6552d7c823c
Mariana Dominguez <maridoh@gmail.com>
2020-06-15 15:20:58 -0700

    added Update post route

--- client/src/services/PostsService.js
+++ client/src/services/PostsService.js
@@ -4,8 +4,13 @@ export default {
   fetchPosts () {
     return Api().get('posts')
   },
-
   addPost (params) {
     return Api().post('posts', params)
+  },
+  updatePost (params) {
+    return Api().put('posts/' + params.id, params)
+  },
+  getPost (params) {
+    return Api().get('post/' + params.id)
   }
 }
